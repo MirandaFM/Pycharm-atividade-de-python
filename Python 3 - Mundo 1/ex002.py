4d146f1778893899e405145277e2c56e1a20956b
Fernando Miranda <162756925+MirandaFM@users.noreply.github.com>
2025-01-19 13:12:04 -0300

    Update ex002.py

--- Python 3 - Mundo 1/ex002.py
+++ Python 3 - Mundo 1/ex002.py
@@ -1,4 +1,7 @@
 #Faça um programa que leia o nome de uma pessoa e mostre uma#
 
-nome = input ('qual é seu nome ? ')
-print ('é um prazer te conhecer, {}!'.format(nome))
+nome =input ('qual é seu nome ?')
+print ('é um prazer te conhecer,',nome)
+
+nome1 = input ('qual é seu ultimo nome?')
+print ('nome bonito,{}!'.format(nome1))
